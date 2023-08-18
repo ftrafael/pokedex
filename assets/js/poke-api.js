299add14a2fe8e97bf7cf8dfb5e29f4dea47993d
Rafael <rft.ferreira@gmail.com>
2023-08-18 15:40:18 -0300

    tweaks in css and addition of button in html

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -32,4 +32,4 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
-}
+}       
